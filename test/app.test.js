5b90c2fb1c679328d93c5200885558ae3ac13760
Charlie <heinercj@gmail.com>
2018-04-10 16:32:39 -0700

    set up server.js. wrote first test for app. no app yet

---
+++ test/app.test.js
@@ -0,0 +1,15 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const { assert } = chai;
+const app = require('../lib/app');
+
+describe('app tests', () => {
+    it('responds hello to name when get to route /greeting/name', () => {
+        return chai.request(app)
+            .get('/greeting/Charlie')
+            .then(response => {
+                assert.equal(response.text, 'Hello Charlie');
+            });
+    });
+});
