4eca688b0dd9857f95b12f91e0906930acdf2836
Ryan Manro <rmanro@gmail.com>
2018-04-10 17:07:27 -0700

    seperating test files

--- test/test-app-greeting.js
+++ test/test-app-greeting.js
@@ -4,7 +4,7 @@ chai.use(chaiHttp);
 const { assert } = chai;
 const app = require('../lib/app');
 
-describe('HTTP APP', () => {
+describe('GET - Greeting', () => {
 
     it('GET Says Hello with name (no salutation)', () => {
         return chai.request(app)
@@ -20,6 +20,23 @@ describe('HTTP APP', () => {
             .query('salutation=yo')
             .then(response => {
                 assert.equal(response.text, 'yo Ryan');
+            });
+    });
+
+    it('GET Says Hello without name & salutation', () => {
+        return chai.request(app)
+            .get('/greeting')
+            .then(response => {
+                assert.equal(response.text, 'hello stranger');
+            });
+    });
+
+    it('GET Says salutation without name', () => {
+        return chai.request(app)
+            .get('/greeting')
+            .query('salutation=yo')
+            .then(response => {
+                assert.equal(response.text, 'yo stranger');
             });
     });
 
