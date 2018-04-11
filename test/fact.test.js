694e28d2eab0e7968c4a017d803d9d2edf16ceda
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-11 06:08:32 -0700

    change fact test from regex to Chai's "include"

--- test/fact.test.js
+++ test/fact.test.js
@@ -1,9 +1,10 @@
-const assert = require('assert');
-const fact = require('../lib/fact');
+const chai = require('chai');
+const { assert } = chai;
+const fetchFact = require('../lib/fact');
 
 describe('fact function', () => {
     it('returns an object with a random http fact as a property', () => {
-        const response = fact();
-        assert.ok(/http/i.test(response.fact));
+        const { fact } = fetchFact();
+        assert.include(fact.toLowerCase(), 'http');
     });
 });
