1fb9b0028c52a1206783a30b19d2cdb05e17ba4f
CharlyWelch <blwbiology@gmail.com>
2018-04-11 09:34:03 -0700

    add test for app no params

--- test/app.test.js
+++ test/app.test.js
@@ -3,3 +3,14 @@ const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const { assert } = chai;
 const app = require('../lib/app');
+
+describe('HTTP app', () => {
+
+    it('Says Hello stranger on GET with no params', () => {
+        return chai.request(app)
+            .get('/greeting')
+            .then(response => {
+                assert.equal(response.text, 'Hello stranger!');
+            });
+    });
+});
