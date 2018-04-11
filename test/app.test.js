a6a4c7e95e3934db7e29b79dfefc935597e90f13
Katlyn Tucker <studioquirk@yahoo.com>
2018-04-11 11:37:26 -0700

    cleaned up old comments to merge with master

--- test/app.test.js
+++ test/app.test.js
@@ -1,5 +1,3 @@
-//import { request } from 'http';
-
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
--- test/greeting.test.js
+++ test/greeting.test.js
@@ -1,10 +1,8 @@
-//request that doesn't match a defined `METHOD` and url `path` should return 404 Not Found 
-//and include the text: `CANNOT <METHOD> <path>`
 const greeting = require('../lib/greeting');
 const {assert} = require('chai');
 
 describe('specific greeting', () => {
-
+    
     it('returns specific greeting', () => {
         const response = greeting('Katlyn');
         assert.equal(response, 'hello Katlyn');
@@ -12,3 +10,5 @@ describe('specific greeting', () => {
 });
 
 
+//request that doesn't match a defined `METHOD` and url `path` should return 404 Not Found 
+//and include the text: `CANNOT <METHOD> <path>`
