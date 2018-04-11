1fee4acca61f1ca59de4b6227eeacb55b16c3788
pereztjacob <pereztjacob@gmail.com>
2018-04-11 10:58:44 -0700

    eslint, travis, etc

---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require('./lib/app');
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log('SERVER ON: ', server.address().port);
+});
