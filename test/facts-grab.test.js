1556b4f50adb3235586237482156eb6f0e5c7892
CharlyWelch <blwbiology@gmail.com>
2018-04-10 18:14:55 -0700

    fail factGrab test

--- test/facts-grab.test.js
+++ test/facts-grab.test.js
@@ -2,9 +2,12 @@ const chai = require('chai');
 const factsGrab = require('../lib/facts-grab');
 const { assert } = chai;
 
-describe.skip('Facts Grab', () => {
+describe('Facts Grab', () => {
 
     it('gets a random fact about http', () => {
-
+        factsGrab()
+            .then(response => {
+                assert.ok(/http/.test(response));
+            });
     });
 });
