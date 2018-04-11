333072281e8845518ea8c54801caa1f25e3ba3d6
Katlyn Tucker <studioquirk@yahoo.com>
2018-04-11 00:46:26 -0700

    set up server, wrote first test for response

--- test/app.test.js
+++ test/app.test.js
@@ -1,16 +1,19 @@
+//import { request } from 'http';
+
 const chai = require('chai');
-const chaiHttp = require('chaiHttp';
+const chaiHttp = require('chaiHttp');
 chai.use(chaiHttp);
 const {assert} = chai;
 const app = require('./lib/app');
+//const server = require('./lib/server');
 
+
+//returns a greeting on GET
 describe('http app', () => {
-    it('returns a greeting on GET /', () => {
-        
-    })
-
-
-
-
-
-})
+    
+    return chai.request(app)
+        .get('/')
+        .then(response => {
+            assert.deepEqual(response.body, 'hello stranger');
+        });
+});
---
+++ test/greeting.test.js
@@ -0,0 +1,2 @@
+//request that doesn't match a defined `METHOD` and url `path` should return 404 Not Found 
+//and include the text: `CANNOT <METHOD> <path>`
