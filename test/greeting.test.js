99ef811ad6e5d08161d6c1e584e2bb7be00589a8
CharlyWelch <blwbiology@gmail.com>
2018-04-11 09:29:43 -0700

    refactor greeting to take a query string for salutation

--- test/greeting.test.js
+++ test/greeting.test.js
@@ -5,13 +5,17 @@ const { assert } = chai;
 describe('Greeting Test', () => {
 
     it('greets with hello stranger if no name', () => {
-        const hiya = greeting();
+        const hiya = greeting('/greeting');
         assert.equal(hiya, 'Hello stranger!');
     });
     
     it('greets with name if provided', () => {
-        const ohItsYou = greeting('Norman');
+        const ohItsYou = greeting('/greeting/Norman');
         assert.equal(ohItsYou, 'Hello Norman!');
     });
-
+    
+    it('greets with name and salutation if provided', () => {
+        const yoItsYou = greeting('/greeting/Norman', { salutation: 'Yo' });
+        assert.equal(yoItsYou, 'Yo Norman!');
+    });
 });
