c4e99fa0095300744535f123e310959aedc871ec
Katlyn Tucker <studioquirk@yahoo.com>
2018-04-10 18:52:17 -0700

    git problem

---
+++ test/app.test.js
@@ -0,0 +1,16 @@
+const chai = require('chai');
+const chaiHttp = require('chaiHttp';
+chai.use(chaiHttp);
+const {assert} = chai;
+const app = require('./lib/app');
+
+describe('http app', () => {
+    it('returns a greeting on GET /', () => {
+        
+    })
+
+
+
+
+
+})
