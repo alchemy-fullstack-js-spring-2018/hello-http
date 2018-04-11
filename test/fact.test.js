2b512ee6e8c5ea528995ec9672518dc0d8ff0fee
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-11 10:56:10 -0700

    wrote test to check random fact

--- test/fact.test.js
+++ test/fact.test.js
@@ -1,12 +1,22 @@
-const { assert } = require('chai');
+const { expect } = require('chai');
 const fact = require('../lib/fact');
 
 describe('get fact', ()  => {
+
+    const facts = [
+        {
+            fact: 'fact 1'
+        },
+        {
+            fact: 'fact 2'
+        },
+        {
+            fact: 'fact 3'
+        }
+    ];
     
     it.only('gets fact 1', () => {
-        assert.deepEqual(fact(), {
-            fact: 'fact 1'
-        });
+        expect(facts).to.deep.include(fact());
     });
 
 });
