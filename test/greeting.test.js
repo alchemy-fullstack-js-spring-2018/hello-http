4f99834bd7eb562561b7f82a4af0874fadafe131
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-10 17:07:44 -0700

    greeting function greets

--- test/greeting.test.js
+++ test/greeting.test.js
@@ -5,4 +5,9 @@ describe('greeting', () => {
     it('defaults to "hello, stranger"', () => {
         assert.strictEqual(greeting(), 'hello, stranger');
     });
+
+    it('provides a custom greeting if given arguments', () => {
+        const customized = greeting('howdy', 'keli');
+        assert.strictEqual(customized, 'howdy, keli');        
+    });
 });
