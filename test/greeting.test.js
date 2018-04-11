18b8588ba4aa46998e1dc29d92c6e87736e50742
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-04-10 17:29:52 -0700

    greeting wroks, start facts

---
+++ test/greeting.test.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const { assert } = chai;
+const app = require('../lib/app');
+
+describe('http app', () => {
+    it('says custom greeting on /greeting/<name>', () => {
+        return chai.request(app)
+            .get('/greeting/george?salutation=hey')
+            .then(response => {
+                assert.equal(response.text, 'hey george');
+            });
+    });
+    it('says stranger if no name given', () => {
+        return chai.request(app)
+            .get('/greeting/?salutation=sup')
+            .then(response => {
+                assert.equal(response.text, 'sup stranger');
+            });
+    });
+    it('greets with hello if no salutation', () => {
+        return chai.request(app)
+            .get('/greeting/frodo')
+            .then(response => {
+                assert.equal(response.text, 'hello frodo');
+            });
+    });
+});
