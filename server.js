4dbc0b7a09124a37f5e0f15a1ac9c0e1dd1c96d4
J3ffcon1 <Jrlone1990@gmail.com>
2018-04-11 13:02:36 -0700

    Getting code setup, following lecture

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const app = require('./lib/app');
 const http = require('http');
 
-// same thing as below:
-// const Server = http.Server;
-// const server = new Server();
-// server.on('request', app);
 
-const PORT = process.env.PORT || 3000;
+
+// // same thing as below:
+// // const Server = http.Server;
+// // const server = new Server();
+// // server.on('request', app);
+
+// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; //on heroku it will set a port based on env.
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-    // eslint-disable-next-line
-    console.log('server started', server.address().port);
-});
+module.exports = server.listen(PORT, () => { //we now can tell the app test there is a server.
+   //eslint-disable-next-line
+    console.log('server has started', server.address().port ); //server.listen starts it.
+} )
+
+
--- test/app-test.js
+++ test/app-test.js
@@ -1,74 +1,33 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
-const { assert } = chai;
-const app = require('../lib/app');
+const { assert } = chai; //equivelent to const assert = chai.assert
+const app = require('../lib/app.js')
 
-describe('http app', () => {
+describe('http app', () =>{
 
-    it('says hello world on GET /', () => {
-        // Pass app to chai.request, which will start (call .listen())
-        // and auto-close server after request
+    it('says hello world on GET /', () =>{
+        //pass app to chai.request, which will start with call.listen()
+        //then we want to auto close the server after the request.
         return chai.request(app)
-            // method and path
+            //method and path
             .get('/')
             .then(response => {
-                assert.equal(response.text, 'hello world');
+                assert.equal(response.text, 'hello world'); //response.text returns a string.
             });
     });
 
-    it('says custom greeting on GET / with query', () => {
+    it('returns dog object on GET /dog', () =>{
+        
         return chai.request(app)
-            .get('/')
-            .query('salutation=yo')
-            .then(({ text }) => {
-                assert.equal(text, 'yo world');
-            });
-    });
-
-    it('returns cat object on GET /cats/garfield', () => {
-        return chai.request(app)
-            .get('/cats/garfield')
+            .get('/dog')
             .then(response => {
-                assert.deepEqual(response.body, {
-                    name: 'garfield',
-                    type: 'orange tabby'
+                assert.deepEqual(response.body, { //response.body returns an object.
+                    name: 'Homer',
+                    type: 'Staffordshire Terrier'
                 });
             });
-    });
 
-    it('returns all cats on GET /cats', () => {
-        return chai.request(app)
-            .get('/cats')
-            .then(response => {
-                assert.equal(response.body.length, 4);
-            });
-    });
 
-    it('return 404 on bad path', () => {
-        return chai.request(app)
-            .get('/bad')
-            .then(
-                // success handler
-                () => {
-                    throw new Error('unexpected successful response');
-                },
-                // failure handler
-                response => {
-                    assert.equal(response.status, 404);
-                    // assert.equal(response.text, 'Sorry...')
-                }
-            );
-    });
-
-    it('echos POST to /echo', () => {
-        const obj = { foo: true };
-
-        return chai.request(app)
-            .post('/echo')
-            .send(obj)
-            .then(({ body }) => {
-                assert.deepEqual(body, obj);
-            });
     });
 });
