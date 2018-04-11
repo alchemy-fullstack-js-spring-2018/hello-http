036e001e12bc5f9021993f788be7f01d08346fb6
Ryan Manro <rmanro@gmail.com>
2018-04-10 21:23:22 -0700

    FACT testing OK

--- test/test-app-fact.js
+++ test/test-app-fact.js
@@ -3,7 +3,6 @@ const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const { assert } = chai;
 const app = require('../lib/app');
-const facts = require('../lib/facts');
 
 describe('GET - Fact', () => {
 
@@ -11,7 +10,7 @@ describe('GET - Fact', () => {
         return chai.request(app)
             .get('/fact')
             .then(response => {
-                assert.ok(/http/.test(response.toString()));
+                assert.ok(/http/.test(response.text));
             });
     });
 
