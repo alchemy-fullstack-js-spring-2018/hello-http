e5ac0053490698fa6061467653595c9a0de6b3ee
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-11 09:38:41 -0700

    wrote app test for custom greeting

--- test/app.test.js
+++ test/app.test.js
@@ -6,11 +6,20 @@ const app = require('../lib/app');
 
 describe(('http app'), () => {
     
-    it('say hello world on GET /', () => {
+    it('say default greeting hello stranger on GET /', () => {
         return chai.request(app)
             .get('/')
             .then(response => {
                 assert.equal(response.text, 'hello stranger');
+            });
+    });
+
+    it('says custom greeding on GET / with query', () => {
+        return chai.request(app)
+            .get('/')
+            .query('salutation=what%20up?name=steve')
+            .then(response => {
+                assert.equal(response.text, 'what up steve');
             });
     });
 });
