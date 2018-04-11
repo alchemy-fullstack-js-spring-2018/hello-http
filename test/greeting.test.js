f720261c5c1e1adabdb8a3426a2bb5328f072d9f
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-11 09:27:55 -0700

    wrote failing test

--- test/greeting.test.js
+++ test/greeting.test.js
@@ -6,4 +6,8 @@ describe('greetings function', () => {
     it.only('defalut says hello stranger', () => {
         assert.equal(greeting(), 'hello stranger');
     });
+
+    it.only('custom greeting', () => {
+        assert.equal(greeting('what up', 'steve'), 'hello freind');
+    });
 });
