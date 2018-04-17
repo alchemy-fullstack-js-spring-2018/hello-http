0c3b514599ade414666deec1ef643ece8df794b4
Katlyn Tucker <studioquirk@yahoo.com>
2018-04-16 19:08:09 -0700

    gets general and specific greeting, return specific greeting, 3 tests passing

--- test/app.test.js
+++ test/app.test.js
@@ -3,11 +3,20 @@ const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const {assert} = chai;
 const app = require('../lib/app');
-//const request = chai.request(app);
+//const greeting = require('../lib/greeting');
 
 describe('returns greeting on GET / ', () => {
+
+    it('gets general greeting', () => {
+        return chai.request(app)
+            //method and path
+            .get('/')
+            .then(response => {
+                assert.equal(response.text, 'hello stranger');
+            });
+    });
     
-    it('gets greeting', () => {
+    it('gets specific greeting', () => {
         return chai.request(app)
             //method and path
             .get('/greeting')
