26e52e664d662a71a0a5a78f95094bcfe4d0ddfd
Jack Lomax <lomax15@me.com>
2018-04-18 13:59:38 -0700

    changed up syntax and ran functional custom greeting and sal test

--- test/app.test.js
+++ test/app.test.js
@@ -5,11 +5,20 @@ const { assert } = chai;
 const app = require('../lib/app');
 
 describe('tests', () => {
-    it('responds with go blazers', () => {
+    it('responds with greeting', () => {
         return chai.request(app)
-            .get('/greeting')
+            .get('/greeting/Jack')
             .then(response => {
-                assert.equal(response.text, 'Go blazers');
+                assert.equal(response.text, 'Hello Jack');
             });  
+    });
+
+    it('responds with custom greeting and salutation', () => {
+        return chai.request(app)
+            .get('/greeting/jack')
+            .query('salutation=hey')
+            .then(({ text }) => {
+                assert.equal(text, 'hey jack');
+            });
     });
 });
