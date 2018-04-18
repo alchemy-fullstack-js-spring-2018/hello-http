e81210edbf7926759f17a41db087430e658b2fa0
Jack Lomax <lomax15@me.com>
2018-04-18 14:09:35 -0700

    random fact works and test passes

--- test/app.test.js
+++ test/app.test.js
@@ -3,9 +3,10 @@ const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const { assert } = chai;
 const app = require('../lib/app');
+var expect = chai.expect;
 
 describe('tests', () => {
-    it('responds with greeting', () => {
+    it('greeting', () => {
         return chai.request(app)
             .get('/greeting/Jack')
             .then(response => {
@@ -13,12 +14,20 @@ describe('tests', () => {
             });  
     });
 
-    it('responds with custom greeting and salutation', () => {
+    it('custom greeting and salutation', () => {
         return chai.request(app)
             .get('/greeting/jack')
             .query('salutation=hey')
             .then(({ text }) => {
                 assert.equal(text, 'hey jack');
+            });
+    });
+
+    it('random http fact', () => {
+        return chai.request(app)
+            .get('/fact')
+            .then(response => {
+                expect(response.body).to.have.property('fact');
             });
     });
 });
